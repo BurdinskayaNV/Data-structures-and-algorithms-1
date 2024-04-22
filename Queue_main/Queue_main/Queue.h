0349a7450dc6bcbf0018d634426614935700ae11
Наталья <148595309+BurdinskayaNV@users.noreply.github.com>
2024-04-22 05:35:20 +0300

    Add files via upload

---
+++ Queue_main/Queue_main/Queue.h
@@ -0,0 +1,172 @@
+#pragma once
+#include "Spisok_one.h"    // односвязный список с прошлого задания
+#include "Spisok_class.h"  // двусвязный список с прошлого задания
+#include <iostream> //объявляет cin cout
+#include <sstream>  //потоковой работы со строчным типом данных
+#include <cassert>  //для assert
+#include <locale>   //определяет функцию setlocale для установки русской локали
+
+
+// Класс Queue есть представитель структуры данных - очередь (FIFO)
+// param Type тип элементов очереди
+template <typename Type>
+// наследование от двусвязного списка DoublyList
+class Queue : private DoublyList<Type>
+{
+public:
+
+    // Конструктор по умолчанию
+    Queue() {}
+
+    // Деструктор
+    ~Queue() {}
+
+    // Добавление элемента в очередь
+    // param element Добавляемый элемент
+    // BigO: O(1)
+    void AddQueue(Type element)
+    {
+        this->AddFront(element);
+    }
+
+    // Удаление элемента из очереди, удалится первый вошедший, вернется значение по умолчанию для Type
+    // BigO: O(1)
+    Type DelQueue()
+    {
+        // Если стек пуст
+        if (this->isEmpty())
+        { return Type(); } //вернется значение по умолчанию для Type
+         
+        Type val = this->get_OneList(this->length() - 1)->data;
+        this->DelBack();
+        return val;
+    }
+
+    // Проверяет пуста ли очередь
+    // return Значение true||false
+    // BigO: O(1)
+    bool isEmpty() {
+        bool empty = false;
+        if (this->length() == 0)
+            empty = true;
+        return empty;
+    }
+
+    // Вернет размер очереди
+    // return size_t - размер
+    size_t size() {
+        return this->length();
+    }
+
+    // Вернет значение конца очереди
+    // return Type
+    Type getTailData() {
+        return this->get_OneList(0)->data;
+    }
+
+    // Вернет значение начала очереди
+    // return Type
+    Type getHeadData() {
+        return this->get_OneList(this->size() - 1)->data;
+    }
+
+    // Возращает элемент очереди, готовый к выходу из нее
+    // return Элемент, если список пуст, то 0
+    // BigO: O(1)
+    Type peek() {
+        if (this->isEmpty())
+        {
+            return 0;
+        }
+        return this->get_OneList(this->length() - 1)->data;
+    }
+};
+
+// Функция тестирования метода AddQueue
+void Test_AddQueue() {
+    Queue<int> queue;
+
+    queue.AddQueue(1);
+    assert(queue.getTailData() == 1);
+
+    queue.AddQueue(2);
+    assert(queue.getTailData() == 2);
+    
+    queue.AddQueue(3);
+    assert(queue.getHeadData() == 1);
+    assert(queue.getTailData() == 3);
+    cout << "-- Тестирование AddQueue выполнено успешно -------" << endl;
+}
+
+// Функция тестирования метода DelQueue
+void Test_DelQueue() {
+    Queue<int> queue;
+
+    //добавляем
+    queue.AddQueue(1);
+    queue.AddQueue(2);
+    queue.AddQueue(3);
+
+    //проверяем
+    queue.DelQueue();
+    assert(queue.size() == 2);
+    assert(queue.getHeadData() == 2);
+    assert(queue.getTailData() == 3);
+
+    queue.DelQueue();
+    assert(queue.size() == 1);
+    assert(queue.getHeadData() == 3);
+    assert(queue.getTailData() == 3);
+
+    assert(queue.DelQueue() == 3);
+    assert(queue.size() == 0);
+
+    //проверка на ассерт пустой очереди double
+    Queue<double> queueEmpty;
+    assert(queueEmpty.DelQueue() == 0.0);
+
+    //проверка на ассерт пустой очереди строк
+    Queue<string> queueEmpty2;
+    assert(queueEmpty2.DelQueue() == "");
+    cout << "-- Тестирование DelQueue выполнено успешно -------" << endl;
+}
+
+// Тестирование функции Peek
+void Test_Peek() {
+    Queue<int> queue;
+
+    queue.AddQueue(1);
+    queue.AddQueue(2);
+    queue.AddQueue(3);
+
+    assert(queue.peek() == 1);
+
+    queue.DelQueue();
+    assert(queue.peek() == 2);
+
+    queue.AddQueue(123);
+    assert(queue.peek() == 2);
+    cout << "-- Тестирование Peek выполнено успешно -----------" << endl;
+}
+
+// Тестирование функции isEmpty
+void Test_IsEmpty() {
+    Queue<int> queue;
+    assert(queue.isEmpty());
+
+    queue.AddQueue(123);
+    assert(!queue.isEmpty());
+
+    queue.DelQueue();
+    assert(queue.isEmpty());
+    cout << "-- Тестирование isEmpty выполнено успешно --------" << endl;
+}
+
+// Функция тестирования класса Stack
+void Test_Queue() 
+{
+    Test_AddQueue();
+    Test_DelQueue();
+    Test_Peek();
+    Test_IsEmpty();
+}
---
+++ Queue_main/Queue_main/Queue_main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "Queue.h"
+
+using namespace std;
+
+int main()
+{
+    // «Rus» локализация произойдёт на русский язык
+    setlocale(LC_ALL, "russian");
+    cout << endl;
+    // Тесты ASSERT
+    Test_Queue();
+    cout << endl;
+
+    Queue<int> queue1;
+    // добавили элементы в первую очередь
+    queue1.AddQueue(18);
+    queue1.AddQueue(28);
+    queue1.AddQueue(38);
+ 
+    cout << "queue1 : " << endl;
+    cout << queue1.peek() << " Peek() Возращает элемент очереди, готовый к выходу из нее" << endl;
+    cout << queue1.DelQueue() << " был удален из queue1" << endl;
+    cout << queue1.size() << " длина очереди queue1" << endl;
+    queue1.AddQueue(48);
+    cout << queue1.getTailData() << " добавлен новый элемент в queue1 " << endl;
+    cout << queue1.size() << " длина очереди queue1 стала после добавления " << endl;
+
+    return 0;
+}
